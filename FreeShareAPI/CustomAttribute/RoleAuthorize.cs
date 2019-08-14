810ea6453a23259bbaf97007d77f45586bec0e41
sunil <sunil@codebee.dk>
2019-08-14 16:56:03 +0545

    WAC-6 custom authentication attribute

--- FreeShareAPI/CustomAttribute/RoleAuthorize.cs
+++ FreeShareAPI/CustomAttribute/RoleAuthorize.cs
@@ -1,90 +1,41 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using FreeShareAPI.Common;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Principal;
-using System.Text;
+using System.Net;
+using System.Net.Http;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Http.Controllers;
 
 namespace FreeShareAPI.CustomAttribute
 {
     /// <summary>
     /// 
     /// </summary>
-    public class RoleAuthorize:AuthorizeAttribute
+    public class RoleAuthorize : AuthorizeAttribute
     {
-        string secretkey = ConfigurationManager.AppSettings["Secret"];
-
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="filterContext"></param>
-        public override void OnAuthorization(AuthorizationContext filterContext)
-        {
-
-            bool result = false;
-            HttpContext httpContext = HttpContext.Current;
-            string authHeader = httpContext.Request.Headers["Authorization"];
-            if (authHeader != null && authHeader.StartsWith("bearer", StringComparison.OrdinalIgnoreCase))
-            {
-                var tokenStr = authHeader.Substring("Bearer ".Length).Trim();
-                result = ValidateToken(tokenStr);
-            }
-            if (result)
-            {
-
-            }
-            else
-            {
-
-            }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="authToken"></param>
-        /// <returns></returns>
-
-        public bool ValidateToken(string authToken)
+        /// <param name="actionContext"></param>
+        public override void OnAuthorization(HttpActionContext actionContext)
         {
             try
             {
-                var result = false;
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var validationParameters = GetValidationParameters();
-                var lifeTime = new JwtSecurityTokenHandler().ReadToken(authToken).ValidTo;
-                if (lifeTime > DateTime.UtcNow)
+                bool response = false;
+                response = new Security().CheckRequestHeader();
+                if (!response)
                 {
-                    SecurityToken validatedToken;
-                    IPrincipal principal = tokenHandler.ValidateToken(authToken, validationParameters, out validatedToken);
-                    result = true;
+                    actionContext.Response =
+                        actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Invalid API call");
                 }
-                return result;
             }
             catch (Exception ex)
             {
-                return false;
+                actionContext.Response =
+                                actionContext.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error");
             }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public TokenValidationParameters GetValidationParameters()
-        {
-            return new TokenValidationParameters()
-            {
-                ValidateLifetime = true, // Because there is no expiration in the generated token
-                ValidateAudience = false, // Because there is no audiance in the generated token
-                ValidateIssuer = false,   // Because there is no issuer in the generated token
-                ValidIssuer = "Sample",
-                ValidAudience = "Sample",
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretkey)) // The same key as the one that generate the token
-            };
         }
     }
 }
